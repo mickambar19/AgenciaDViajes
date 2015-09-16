0d087995c947bf0678ded1f0e782f08dd8fb57e1
mickambar19-Ino <mickambar19@gmail.com>
2015-09-16 17:27:55 -0500

    Quedo finalizada la clase Destinos por si se necesitara

--- destino.cpp
+++ destino.cpp
@@ -1,5 +1,51 @@
 #include "destino.h"
 
+
+int Destino::getCodigoDestino() const
+{
+    return codigoDestino;
+}
+
+void Destino::setCodigoDestino(int value)
+{
+    codigoDestino = value;
+}
+
+string Destino::getNombreDestino() const
+{
+    string nombreDestinoString;
+    nombreDestinoString.assign(nombreDestino);
+    return nombreDestinoString;
+}
+
+void Destino::setNombreDestino(string value)
+{
+    strcpy(nombreDestino , value.c_str());
+}
+
+string Destino::getTipoDestino() const
+{
+    string tipoDestinoString;
+    tipoDestinoString.assign(tipoDestino);
+    return tipoDestinoString;
+}
+
+void Destino::setTipoDestino(string value)
+{
+    strcpy(tipoDestino , value.c_str());
+}
+
+string Destino::getEstatusDestino() const
+{
+    string estatusDestinoString;
+    estatusDestinoString.assign(tipoDestino);
+    return estatusDestinoString;
+}
+
+void Destino::setEstatusDestino(string value)
+{
+    strcpy(estatusDestino , value.c_str());
+}
 Destino::Destino()
 {
 
--- destino.h
+++ destino.h
@@ -1,11 +1,27 @@
 #ifndef DESTINO_H
 #define DESTINO_H
-
-
+#include <iostream>
+#include <cstring> //Para copiar string to char y comparar
+#define MAX_CARACTERES 35
+using namespace std;
 class Destino
 {
+private:
+    int codigoDestino;
+    char nombreDestino[MAX_CARACTERES];
+    char tipoDestino[MAX_CARACTERES];
+    char estatusDestino[MAX_CARACTERES];
 public:
     Destino();
+
+    int getCodigoDestino() const;
+    void setCodigoDestino(int value);
+    string getNombreDestino() const;
+    void setNombreDestino(string value);
+    string getTipoDestino() const;
+    void setTipoDestino(string value);
+    string getEstatusDestino() const;
+    void setEstatusDestino(string value);
 };
 
 #endif // DESTINO_H
